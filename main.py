11a44aff8f3dad4d9fc5a028941672b4e24210cb
jcart1283 <joshua.c.carter@outlook.com>
2025-07-17 02:27:13 -0700

    cleaned up output - more human readable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import num_words
 from stats import character_count
+from stats import character_sort
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,10 +8,17 @@ def get_book_text(filepath):
 
 
 def main():
-    book_text = get_book_text('books/frankenstein.txt')
+    book_source ='books/frankenstein.txt'
+    book_text = get_book_text(book_source)
     wordcount = num_words(book_text)
-    print(f'{wordcount} words found in the document')
-    print(character_count(book_text))
+    print("============ BOOKBOT ============")
+    print(f'Analyzing book found at {book_source}...')
+    print("----------- Word Count ----------")
+    print(f'Found {wordcount} total words')
+    print("--------- Character Count -------")
+    character_count_list = (character_sort(character_count(book_text)))
+    for i in character_count_list:
+        print(f'{i["char"]}: {i["num"]}')
 
 
 
--- stats.py
+++ stats.py
@@ -11,3 +11,14 @@ def character_count(book_text):
         else:
             count[l] = 1
     return count
+
+def sort_on(item):
+    return item["num"]
+
+def character_sort(char_dict):
+    output_list = [{"char": c, "num": n} for c, n in char_dict.items()]
+    output_list.sort(reverse=True, key=sort_on)
+#   output_list = [f'"char" : "{c}", "num" : "{char_dict[c]}"']
+#    for c in char_dict:
+#        output_dict.append(f'"char" : "{c}", "num" : "{char_dict[c]}"')
+    return output_list
